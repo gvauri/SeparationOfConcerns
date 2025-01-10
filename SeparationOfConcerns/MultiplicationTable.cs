425defab6068714090013330852d6a0f46c304cc
p20669 <aurel.schmid@css.ch>
2025-01-10 10:48:52 +0100

    MultiplicationTable refactorec

--- SeparationOfConcerns/MultiplicationTable.cs
+++ SeparationOfConcerns/MultiplicationTable.cs
@@ -4,21 +4,14 @@ public class MultiplicationTable
 {
     public static void For(List<int> numbers)
     {
-        // first, let's figure out the biggest value
-        var biggest = int.MinValue;
-        foreach (var number in numbers)
-        {
-            if (number < 0)
-            {
-                throw new ArgumentException("negative numbers are not supported");
-            }
-            if (number > biggest)
-            {
-                biggest = number;
-            }
-        }
+        var table = CalculateMultiplicationTable(numbers);
 
-        // then, find the biggest possible result to compute its magnitude
+        PrintMultiplicationTable(numbers, table);
+    }
+
+    private static void PrintMultiplicationTable(List<int> numbers, List<List<int>> table)
+    {
+        var biggest = numbers.Max();
         var biggestResult = biggest * biggest;
         var magnitude = 0;
         while (biggestResult > 0)
@@ -26,30 +19,44 @@ public class MultiplicationTable
             magnitude++;
             biggestResult /= 10;
         }
-        magnitude++; // the magnitude (plus some space) will be used for the width
+        magnitude++; 
 
-        // finally, create the multiplication table, nicely formatted
-        var titleRow = "";
-        titleRow += "*".PadLeft(magnitude) + " ||";
+        var titleRow = "*".PadLeft(magnitude) + " ||";
         foreach (var col in numbers)
         {
             titleRow += $"{col}".PadLeft(magnitude) + " |";
         }
         Console.WriteLine(titleRow);
+
         for (var i = 0; i < titleRow.Length; i++)
         {
             Console.Write("=");
         }
         Console.WriteLine();
-        foreach (var row in numbers)
+
+        foreach (var row in table)
         {
-            Console.Write($"{row}".PadLeft(magnitude) + " ||");
-            foreach (var col in numbers)
+            Console.Write($"{numbers[table.IndexOf(row)]}".PadLeft(magnitude) + " ||");
+            foreach (var product in row)
             {
-                var product = row * col;
                 Console.Write($"{product}".PadLeft(magnitude) + " |");
             }
             Console.WriteLine();
         }
     }
+    private static List<List<int>> CalculateMultiplicationTable(List<int> numbers)
+    {
+        var table = new List<List<int>>();
+        foreach (var row in numbers)
+        {
+            var rowValues = new List<int>();
+            foreach (var col in numbers)
+            {
+                rowValues.Add(row * col);
+            }
+            table.Add(rowValues);
+        }
+        return table;
+    }
+
 }
